55816176a4562e367a83e71608360d1e52d7d5de
CYS0131 <jack879tw@gmail.com>
2017-06-06 23:38:53 +0800

    Add files via upload

---
+++ test.js
@@ -0,0 +1,9 @@
+var assert = require("assert")
+var scraper = require("./scraper.js")
+describe("查詢台灣及時天氣" , function () {
+    describe("#scraper" , function()){
+        it("test scraper" , function()){
+            assert.equal(url,body)
+        }
+    }
+})
